76b010a050ebf381b385f875592d1cfe22b54f99
Kamil Lipiński <kamil@appdiamond.pl>
2018-07-31 13:46:09 +0200

    Add form row component and input component.

--- resources/assets/js/components/CRUD/common/Form.vue
+++ resources/assets/js/components/CRUD/common/Form.vue
@@ -1,29 +1,17 @@
 <template>
     <b-form :validated="validated" @submit.prevent="$emit('submit')" novalidate>
-        <b-row class="my-2">
-            <b-col sm="2"><label for="name">Name:</label></b-col>
-            <b-col sm="10" md="6">
-                <b-form-input autocomplete="email" v-validate="this.rules.name" :state="errors.has('name') ? false : null"
-                              name="name" id="name" type="text" v-model="record.name"></b-form-input>
-                <div class="invalid-feedback" v-show="errors.has('name')">{{ errors.first('name') }}</div>
-            </b-col>
-        </b-row>
-        <b-row class="my-2">
-            <b-col sm="2"><label for="email">E-mail:</label></b-col>
-            <b-col sm="10" md="6">
-                <b-form-input autocomplete="email" v-validate="this.rules.email" :state="errors.has('email') ? false : null"
-                              id="email" name="email" type="text" v-model="record.email"></b-form-input>
-                <div class="invalid-feedback" v-show="errors.has('email')">{{ errors.first('email') }}</div>
-            </b-col>
-        </b-row>
-        <b-row class="my-2" v-if="password">
-            <b-col sm="2"><label for="password">Password:</label></b-col>
-            <b-col sm="10" md="6">
-                <b-form-input autocomplete="new-password" v-validate="this.rules.password" :state="errors.has('password') ? false : null"
-                              id="password" name="password" type="password" v-model="record.password"></b-form-input>
-                <div class="invalid-feedback" v-show="errors.has('password')">{{ errors.first('password') }}</div>
-            </b-col>
-        </b-row>
+        <form-row>
+            <label slot="label" :for="record.name">Name:</label>
+            <form-input slot="component" name="name" :validationRules="this.rules.name" v-model="record.name"></form-input>
+        </form-row>
+        <form-row>
+            <label slot="label" :for="record.email">E-mail:</label>
+            <form-input autocomplete="email" slot="component" type="email" name="email" :validationRules="this.rules.email" v-model="record.email"></form-input>
+        </form-row>
+        <form-row v-if="password">
+            <label slot="label" :for="record.password">Password:</label>
+            <form-input autocomplete="new-password" slot="component" type="password" name="password" :validationRules="this.rules.password" v-model="record.password"></form-input>
+        </form-row>
         <b-row class="my-2">
             <b-col sm="12" md="8">
                 <div class="alert alert-warning" role="alert" v-if="validated && errors.any()">
@@ -36,6 +24,9 @@
 </template>
 
 <script>
+    import FormRow from './FormRow';
+    import FormInput from '../../common/form/Input';
+
     export default {
         inject: ['$validator'],
         props: {
@@ -52,6 +43,10 @@
                 type: Boolean,
                 default: false
             }
+        },
+        components: {
+            FormInput,
+            FormRow
         }
     }
 </script>
---
+++ resources/assets/js/components/CRUD/common/FormRow.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-row class="my-2">
+        <b-col sm="2">
+            <slot name="label"></slot>
+        </b-col>
+        <b-col sm="10" md="6">
+            <slot name="component"></slot>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+    export default {
+        props: {
+            for: {
+                type: String,
+                default: null
+            },
+            label: String
+        }
+    }
+</script>
