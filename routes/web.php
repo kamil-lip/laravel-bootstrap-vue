bc903194180895e51afb3e131348e3eb1c876565
Kamil Lipiński <kamil@appdiamond.pl>
2018-08-03 16:49:38 +0200

    Protect CRUD routes with auth middleware.

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,12 @@ $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
-// CRUD
-Route::resource('users','UserController')->only([
-    'index', 'edit', 'show', 'create'
-]);;
+Route::middleware('auth')->group(function () {
+    // CRUD
+    Route::resource('users', 'UserController')->only([
+        'index',
+        'edit',
+        'show',
+        'create'
+    ]);
+});
